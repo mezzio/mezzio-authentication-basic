0b9d57c34a6ecd247504887eb26851c1e95e376b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:01:35 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- test/BasicAccessTest.php
+++ test/BasicAccessTest.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/mezzio/mezzio-authentication-basic for the canonical source repository
- * @copyright https://github.com/mezzio/mezzio-authentication-basic/blob/master/COPYRIGHT.md
- * @license   https://github.com/mezzio/mezzio-authentication-basic/blob/master/LICENSE.md New BSD License
- */
-
 namespace MezzioTest\Authentication\Basic;
 
 use Mezzio\Authentication\AuthenticationInterface;
